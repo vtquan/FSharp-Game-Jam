ee58f7ac17d61fc3271b454a0a3555c06bde696e
Viquoc Quan <vtquan@outlook.com>
2021-10-30 23:32:49 -0400

    Clean up files

--- GameJam/GameJam.Game/Events.cs
+++ GameJam/GameJam.Game/Events.cs
@@ -12,12 +12,6 @@ namespace GameJam
     public static class Events
     {
 
-        public static readonly EventKey<Vector3> MoveDirectionEventKey = new EventKey<Vector3>();
-        public static readonly EventKey<Vector2> CameraDirectionEventKey = new EventKey<Vector2>();
-        public static readonly EventKey<bool> IsGroundedEventKey = new EventKey<bool>();
-        public static readonly EventKey<float> RunSpeedEventKey = new EventKey<float>();
-        public static readonly EventKey<bool> JumpEventKey = new();
-
         public static readonly EventKey<Tuple<string, Entity>> GameEventKey = new();
         public static readonly EventReceiver<Tuple<string, Entity>> gameListener = new(GameEventKey, EventReceiverOptions.Buffered);
 
@@ -27,11 +21,16 @@ namespace GameJam
         public static readonly EventKey SfxEventKey = new();
         public static readonly EventReceiver sfxEvent = new EventReceiver(SfxEventKey);
 
+        public static readonly EventKey<Vector3> MoveDirectionEventKey = new EventKey<Vector3>();
+        public static readonly EventKey<Vector2> CameraDirectionEventKey = new EventKey<Vector2>();
+        public static readonly EventKey<float> RunSpeedEventKey = new EventKey<float>();
+        public static readonly EventKey<bool> IsGroundedEventKey = new EventKey<bool>();
+        public static readonly EventKey<bool> JumpEventKey = new();
+
 
         public static readonly EventReceiver<Vector3> moveDirectionEvent = new EventReceiver<Vector3>(MoveDirectionEventKey);
         public static readonly EventReceiver<float> runSpeedEvent = new EventReceiver<float>(RunSpeedEventKey);
         public static readonly EventReceiver<bool> isGroundedEvent = new EventReceiver<bool>(IsGroundedEventKey);
-
         public static readonly EventReceiver<bool> jumpEvent = new EventReceiver<bool>(JumpEventKey);
     }
 }
