ba24fbf32ccc6ec90ea5551700c74271e5e6a95d
Viquoc Quan <vtquan@outlook.com>
2021-11-09 21:57:28 -0500

    clean up code

--- GameJam/GameJam.Core/Event.fs
+++ GameJam/GameJam.Core/Event.fs
@@ -24,25 +24,6 @@ module Event =
         | "Start" -> [TitleSceneMsg(Start)]
         | "Restart" -> [Restart]
         | _ -> []
-    
-    let private mapGameEvent ((message, entity) : string * Entity) : GameMsg list = 
-        match message with
-        | "Collect" -> 
-            [GameplaySceneMsg(PlayerMsg(Collision(entity))); GameplaySceneMsg(UiMsg(Increment)); Collect]
-        | "Left" -> [GameplaySceneMsg(PlayerMsg(MoveLeft))]
-        | "Right" -> [GameplaySceneMsg(PlayerMsg(MoveRight))]
-        | "Up" -> [GameplaySceneMsg(PlayerMsg(MoveUp))]
-        | "Down" -> [GameplaySceneMsg(PlayerMsg(MoveDown))]
-        | "Jump" -> [GameplaySceneMsg(PlayerMsg(Jump))]
-        | "Grounded" -> [GameplaySceneMsg(PlayerMsg(Grounded))]
-        | "Airborne" -> [GameplaySceneMsg(PlayerMsg(Airborne))]
-        | "NoMovement" -> [GameplaySceneMsg(PlayerMsg(NoMovement))]
-        | "AttachPlayer" -> [GameplaySceneMsg(PlatformMsg(AttachPlayer(entity)))]
-        | "DetachPlayer" -> [GameplaySceneMsg(PlatformMsg(DetachPlayer(entity)))]
-        | "Goal" -> [Goal]
-        | "Start" -> [TitleSceneMsg(Start)]
-        | "Restart" -> [Restart]
-        | _ -> []
        
     let private TryReceiveAllEvent (eventReceiver : EventReceiver<'a>) =   
         let eventList = (Seq.empty).ToList()
